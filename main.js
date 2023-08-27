cc4e76146546ba95392df703c7d08755cba0308c
abhishek <abhishek@iodparts.com>
2023-08-27 11:12:30 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,102 @@
+import { addNewProducts , getProductDetails , updatePrice,removeProduct } from "./productManager.js";
+
+
+let name;
+
+let price;
+
+let colors = []
+
+
+
+const colorBtn = document.getElementById('colorBtn')
+const submitBtn = document.getElementById('submitBtn');
+let color = document.getElementById('color')
+const productName = document.getElementById('name');
+const productPrice = document.getElementById('price');
+
+const getProductBtn = document.getElementById('getProduct');
+const getProdName = document.getElementById('product-name');
+
+const uodatePriceBtn = document.getElementById('updatePrice');
+const updatePriceOfProdName = document.getElementById('prodToUpdatePrice');
+const newPriceName = document.getElementById('new-price')
+
+//remove product
+const RemoveBtn = document.getElementById('removeBtn');
+const removeProductName = document.getElementById('removeProd');
+
+
+submitBtn.addEventListener('click', (event) => {
+
+
+    event.preventDefault();
+
+    name = productName.value;
+    price = parseInt(productPrice.value);
+
+    if (productName.value) {
+        productName.value = "";
+    }
+    if (productPrice.value) {
+        productPrice.value = "";
+    }
+
+    // let product = {
+
+    //     name,
+    //     price,
+    //     colors,
+    // }
+
+    addNewProducts(name, price, ...colors);
+
+})
+
+colorBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    colors.push(color.value);
+
+    if (color.value) {
+        color.value = "";
+    }
+
+})
+
+//Get product Details
+
+getProductBtn.addEventListener('click' , (event)=>{
+
+    event.preventDefault();
+
+    getProductDetails(getProdName.value);
+    if (getProdName.value) {
+        getProdName.value = "";
+    }
+})
+
+//update the price of the product
+
+uodatePriceBtn.addEventListener('click' , (event)=>{
+
+    event.preventDefault();
+
+    updatePrice(updatePriceOfProdName.value , newPriceName.value);
+    if (updatePriceOfProdName.value) {
+        updatePriceOfProdName.value = "";
+    }
+    if (newPriceName.value) {
+        newPriceName.value = "";
+    }
+})
+// updatePrice("iphone13" , "40000")
+
+
+//remove item
+RemoveBtn.addEventListener('click' ,(event)=>{
+
+    event.preventDefault();
+
+    removeProduct(removeProductName.value);
+})
---
+++ productManager.js
@@ -0,0 +1,100 @@
+"use strict";
+
+let products = [
+
+    {
+        name : "iphone11",
+        price : 40000,
+        colors : ['red' , 'gold'],
+    },
+    {
+        name : "iphone12",
+        price : 55000,
+        colors : ['red' , 'blue'],
+    },
+    {
+        name : "iphone13",
+        price : 65000,
+        colors : ['sapphire' , 'white'],
+    },
+    {
+        name : "iphone14",
+        price : 71000,
+        colors : ['green' , 'red' , 'blue' , 'white'],
+    }
+]
+
+
+let addNewProducts = (ProdName , ProdPrice , ...args)=>{
+
+    let product = {
+        name : ProdName,
+        price : ProdPrice,
+        colors : [...args]
+    }
+    products.push(product);
+
+    console.log(`${product.name} is added...!`);
+    console.log(products)
+}
+
+function getProductDetails(ProdName){
+
+    try{
+        // const product = products.filter((product) =>{
+
+        //     if(product.name === ProdName){
+        //         return product;
+        //     }
+        // }
+        // )
+        // console.log(...product);
+
+        const product = products.find(product => product.name===ProdName)
+
+        if(product === undefined){
+            throw new Error('Please enter valid product name...!');
+        }
+
+        console.log(product);
+
+    }catch(err){
+        alert(err);
+    }
+}
+
+
+function updatePrice(prodName , newPrice){
+
+    products.forEach((product) =>{
+
+        if(product.name === prodName){
+            console.log(`Privious price of product is :${product.price}`);
+            product.price = parseInt(newPrice);
+            console.log(`New price of product is :${product.price}`);
+
+        }
+    })
+}
+
+function removeProduct(prodName){
+
+    products.forEach((product , index) =>{
+
+        
+        if(product.name === prodName){
+            
+            if (index > -1) { // only splice array when item is found
+                products.splice(index, 1); // 2nd parameter means remove one item only
+              }
+              console.log(products);
+              return;
+        }
+    })
+
+    console.log('Product that you want to remove is not valid')
+}
+
+
+
+export{addNewProducts , getProductDetails , updatePrice , removeProduct};
